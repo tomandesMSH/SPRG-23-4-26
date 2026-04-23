12e47bb79cf18fece06f06a8ffcb57b6b588e569
tomandes <tomandes@tuta.io>
2026-04-23 11:34:10 +0200

    program2

---
+++ Program2.cs
@@ -0,0 +1,15 @@
+﻿int[] cisla = new int[5];
+
+for (int cislo = 0; cislo < 5; cislo++)
+{
+    Console.Write("Zadej číslo " + (cislo + 1) + ": ");
+    cisla[cislo] = int.Parse(Console.ReadLine()!);
+}
+
+int max = cisla[0];
+for (int i = 1; i < 5; i++)
+{
+    if (cisla[i] > max) max = cisla[i];
+}
+
+Console.WriteLine("Nejvyšší hodnota je: " + max);
