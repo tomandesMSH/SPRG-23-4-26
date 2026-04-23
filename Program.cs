3927d007b9ad847b6770817eb64b6a515c81f991
tomandes <tomandes@tuta.io>
2026-04-23 11:21:04 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Zadej dvě čísla na nasledujici radky: ");
+int cislo = int.Parse(Console.ReadLine());
+int cislo2 = int.Parse(Console.ReadLine());
+int porovnej(int cislo, int cislo2)
+{
+    if (cislo > cislo2)
+    {
+        return 1;
+    }
+    else if (cislo < cislo2)
+    {
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
+};
+
+switch (porovnej(cislo, cislo2))
+{
+    case 1:
+        Console.WriteLine("Větší číslo je: " + cislo);
+        break;
+    case 2:
+        Console.WriteLine("Větší číslo je: " + cislo2);
+        break;
+    default:
+        Console.WriteLine("Čísla jsou stejná.");
+        break;
+}
